34b94cc221c5888c40af8dc4c42927ffd1e05f0a
jaypajji4444 <43105921+jaypajji4444@users.noreply.github.com>
2020-06-20 18:24:39 +0530

    ADD:Static serve modeifid

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const db=async()=>{
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use("/",express.static("Landing page"))
-app.use("/Dashboard",express.static("Dashboard"))
+app.use("/",express.static("Dashboard"))
+//app.use("/Dashboard",express.static("Dashboard"))
 
 
 
